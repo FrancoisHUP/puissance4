c33dd379f59ec00c24c523d167f8d51f8015af53
francoisHUP <>
2022-01-26 21:53:10 -0500

    ajout test

---
+++ src/test/java/ConnectFour.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConnectFour {
+
+    @Test
+    public void connectFourTest(){
+        // contexte
+        ConnectFour connectFour = new ConnectFour();
+        // attendu
+        Boolean attendu = true;
+        // test
+        assertTrue(attendu);
+    }
+}
